d20543fd068fd685129ab497af031173525ae259
rossa <g00401655@atu.ie>
2024-11-14 12:13:46 +0000

    added service class

--- src/main/java/ie/atu/bookingservice/BookingRepository.java
+++ src/main/java/ie/atu/bookingservice/BookingRepository.java
@@ -3,6 +3,9 @@ package ie.atu.bookingservice;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BookingRepository extends MongoRepository<BookingDetails, String> {
+    List<BookingDetails> findByUserId(String userId);
 }
--- src/main/java/ie/atu/bookingservice/BookingService.java
+++ src/main/java/ie/atu/bookingservice/BookingService.java
@@ -1,4 +1,42 @@
 package ie.atu.bookingservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class BookingService {
+    private BookingRepository bookingRepository;
+    private BookingService bookingService;
+    @Autowired
+    public BookingService(BookingRepository bookingRepository) {
+        this.bookingRepository = bookingRepository;
+    }
+
+    public BookingDetails createBooking(BookingDetails bookingDetails) {
+        bookingDetails.setStatus("Pending");
+        return bookingRepository.save(bookingDetails);
+    }
+
+    public Optional<BookingDetails> getBookingById(String id) {
+        return bookingRepository.findById(id);
+    }
+
+    public List<BookingDetails> getByBookingUserId(String userId) {
+        return bookingRepository.findByUserId(userId);
+    }
+
+    public BookingDetails updateBooking(BookingDetails bookingDetails, BookingDetails updatedBooking) {
+        if(bookingRepository.existsById(bookingDetails.getId())) {
+            updatedBooking.setId(bookingDetails.getId());
+            return bookingRepository.save(updatedBooking);
+        }
+        return null;
+    }
+
+    public void deleteBooking(String id) {
+        bookingRepository.deleteById(id);
+    }
 }
